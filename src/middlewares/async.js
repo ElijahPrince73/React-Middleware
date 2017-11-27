b2e1fb0b0644ec499490699d2f96de0ee338603b
elijahprince73 <elijah@1solutionapps.com>
2017-11-27 10:18:53 -0800

    Handling undrelated promises

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -2,8 +2,12 @@
 export default function ({ dispatch }) {
   // Returns a function then returns another function
   return next => action => {
-    console.log('action', action);
-    
-    next(action)
+    // If the action does not have a .then property
+    // We dont care about it send it on
+    if (!action.payload || !action.payload.then) {
+      return next()
+    }
+
+    console.log('We have a promise', action);
   }
 }
