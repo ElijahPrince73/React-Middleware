b89a672e7344fdbea06c25c993827731b495d814
elijahprince73 <elijah@1solutionapps.com>
2017-11-27 10:38:20 -0800

    Middleware Review

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,13 +1,19 @@
-
-export default function ({ dispatch }) {
-  // Returns a function then returns another function
+export default function({ dispatch }) {
   return next => action => {
-    // If the action does not have a .then property
-    // We dont care about it send it on
+    // If action does not have payload
+    // or, the payload does not have a .then property
+    // we dont care about it, send it on
     if (!action.payload || !action.payload.then) {
-      return next()
+      return next(action);
     }
 
-    console.log('We have a promise', action);
+    // Make sure the action's promise resolves
+    action.payload
+      .then(function(response) {
+        // create a new action with the old type, but
+        // replace the promise with the reponse data
+        const newAction = { ...action, payload: response };
+        dispatch(newAction);
+      });
   }
 }
